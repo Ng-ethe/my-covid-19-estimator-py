412f141cdef874cc4f020c64d08b9c5b16f6f59f
Ng-ethe <ngethe.bernice@students.jkuat.ac.ke>
2020-04-13 23:24:36 +0300

    ammend

--- src/estimator.py
+++ src/estimator.py
@@ -56,21 +56,6 @@ def period_conversion(periodType, timeToElapse):
 
 if __name__ == "__main__":
     pass
-"""
-    data = {
-        'region': {
-            'name': "Africa",
-            'avgAge': 19.7,
-            'avgDailyIncomeInUSD': 5,
-            'avgDailyIncomePopulation': 0.71
-        },
-        'periodType': "days",
-        'timeToElapse': 58,
-        'reportedCases': 674,
-        'population': 66622705,
-        'totalHospitalBeds': 1380614
-    } """
-
 
     data = {'region': {'name': input("Region: "),
                        'avgAge': float(input("Average age: ")),
